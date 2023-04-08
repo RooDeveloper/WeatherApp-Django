aa5ae445d59c513ddba6e9ed6a8ef38e4d45e090
AM AaDil <121432606+RooDeveloper@users.noreply.github.com>
2023-04-08 17:22:30 +0530

    commit

--- projects/projects/settings.py
+++ projects/projects/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8y=x*bvist3!ky6#*d=*!dig=y16n24=xryy-ng6rvbg-$(be&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
